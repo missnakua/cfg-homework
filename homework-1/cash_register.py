a52602b164ca7689de41957997e017f5939676e5
Nana Mensah <missnakuaxo@gmail.com>
2022-10-16 23:38:22 +0100

    Finish Homework

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -1,99 +1,70 @@
-"""
+class Item:
 
-TASK 1
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-Write a class to represent a Cash Register.
-Your class should keep the state of price total and purchased items
-
-Below is a starter code:
-------------------------
-1. You can add new variables and functions if you want to.
-2. You will NEED to add accepted method parameters where required.
-For example, method add_item probably accepts some kind of an item?..
-3. You will need to write some examples of how your code works.
-
-"""
 
 class CashRegister:
 
-    def __init__(self, total_items, total_price, is_full_price=False):
-        """
-        Initialises an empty list to hold purchased items.
-        Compulsory attributes: total_items, total_price
-        Optional attributes: discount
-        20% off items where discount applies.
-        """
-        # is discount = "item is at a discount of 0.80 if it is not full price, as full price equals false"
-        self.total_items = None # {'item': 'price'}
-        self.total_price = 0
-        self.is_full_price = is_full_price
-        self.discount = 0.8 if is_full_price else None
-        self.items = []
+    def __init__(self):
 
-
-    def add_item(self, item, price):
-        return self.items.append(item, price)
-
-
-    def remove_item(self, item):
-        return self.items.remove(item)
-
-
-    def apply_discount(self):
-        """
-        Applies a 20% off discount to items at checkout.
-        """
-        # if self.is_full_price=False:
-        #     self.items * 0.8
-        #     print(f"This item is discounted.")
-        # else:
-        #     print(f"This item is full price")
-
-    def get_total(self, price):
-        """
-        Calculates the total price of items in the Cash Register at checkout.
-        """
-        self.total_price = 0
-        for self.item in self.items:
-            total += bukky_shopping_list.get("price")
-            return total
-
-
-    def show_items(self):
-        """
-        Returns a list of all items in the Cash Register.
-        """
-        for self.item in self.items:
-            print("Item Description:", self.items.get("item")),
-            print(str("Price:",self.pr))
-
-            show_items = customer_item.get("item")
-            return show_items
-
-
-
-    def reset_register(self):
-        """
-        Clears the Cash Register
-        """
-        self.total_items.clear()
         self.total_price = 0
         self.discount = 0
-        self.total_items[:] = []
+        self.total_items = []
 
+    def add_item(self, item):
+        self.total_items.append(item)
+        print(f"{item.name} has been added to your basket.")
 
-# EXAMPLE code run:
-# Instantiate the Cash Register class
-bukky_shopping_list = {"sofa": 82.00, "kettle": 5.00}
-shopping_list = CashRegister()
+    def remove_item(self, item):
+        self.total_items.remove(item)
+        print(f"{item.name} has been removed from your basket.")
 
-bukky_items = CashRegister(total_items=5, total_price=20)
+    def apply_discount(self, discount_amount):
+        self.discount += discount_amount
 
-bukky_items.add_item("watermelon", 8.00)
-print(bukky_items)
+    def get_total(self):
+        self.total_price = 0
+        for item in self.total_items:
+            self.total_price += item.price
+        return self.total_price - self.discount
 
+    def show_items(self):
+        print("Items in current basket:")
+        for item in self.total_items:
+            print(f"Item: {item.name}, Price: {item.price}")
 
-# jess_shopping_list = CashRegister()
-# michelle_shopping_list = CashRegister()
-# azeez_shopping_list = CashRegister()
-# lauren_shopping_list = CashRegister()
+    def reset_register(self):
+        self.total_items = []
+        self.total_price = 0
+        self.discount = 0
+
+bukky_basket = CashRegister()
+jess_basket = CashRegister()
+michelle_basket = CashRegister()
+
+sofa = Item("sofa", 80.00)
+chair = Item("chair", 50.00)
+table = Item("table", 60.00)
+lamp = Item("lamp", 20.00)
+
+bukky_basket.add_item(sofa)
+michelle_basket.add_item(lamp)
+michelle_basket.add_item(table)
+jess_basket.add_item(chair)
+
+michelle_basket.remove_item(lamp)
+
+bukky_basket.show_items()
+michelle_basket.show_items()
+jess_basket.show_items()
+
+bukky_basket.apply_discount(5)
+
+print(f"Bukky's total amount is now £{bukky_basket.get_total()} with a £5 discount.")
+print(f"Bukky and Jess' baskets total to £{bukky_basket.get_total() + jess_basket.get_total()} altogether.")
+
+bukky_basket.reset_register()
+michelle_basket.reset_register()
+jess_basket.reset_register()
--- homework-1/student.py
+++ homework-1/student.py
@@ -1,82 +1,58 @@
-from abc import ABC, abstractmethod
-
 """
 
 TASK 2
 
 Write a base class to represent a student. Below is a starter code.
 Feel free to add any more new features to your class.
-As a minimum, a student has a name, an age and a unique ID.
+As a minimum a student has a name and age and a unique ID.
 
 Create a new subclass from student to represent a concrete student doing a specialization, for example:
 Software Student and Data Science student.
 
 """
 
+
 class Student:
 
-    def __init__(self, name, age, id, subjects, grade):
+    def __init__(self, name, age, id):
         self.name = name
         self.age = age
         self.id = id
-        self.subjects = subjects() # dict()
-        self.grade = grade
+        self.subjects = dict()
 
-
-    def get_grade(self, grade):
-        return self.grade
-
-
-    def add_subject(self, subjects):
-        self.subjects.update(subjects)
-
+    def add_subject(self, subject, grade):
+        self.subjects.update({subject: grade})
 
     def remove_subject(self, subject):
-        self.subjects.popitem({subject})
-
+        self.subjects.pop(subject)
 
     def view_subjects(self):
-        pass
+        print(f"{self.name}'s subject are {self.subjects}")
+
+    def get_overall_mark(self): #Use average
+        avg_mark = sum(self.subjects.values()) / len(self.subjects)
+        print(f"Your average mark is {round(avg_mark,1)}%")
+        return avg_mark
+
+    def update_grade(self, subject, grade):
+        self.subjects.update({subject: grade})
 
 
-    def overall_mark(self): #Use average
-        pass
+class CFGStudent (Student):
+
+    def __init__(self, name, age, id, specialization):
+        super().__init__(name, age, id)
+        self.specialization = specialization
 
 
-class Software(Student):
-    pass
+student_1 = CFGStudent(name= 'Nana', age=23, id=1234, specialization='Software')
 
+student_1.add_subject('Dictionaries', 90)
+student_1.add_subject('Lists', 78)
+student_1.add_subject('Python', 100)
+student_1.view_subjects()
 
-class DataScience(Student):
-    pass
+student_1.get_overall_mark()
 
-# bukky =
-# chido =
-# jess =
-# michelle =
-# lauren =
-# odera =
-
-
-
-
-# class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subects (add/remove new subject and its grade to the dict)
-#     create a method to view all subjects taken by a student
-#     create a method (and a new variable) to get student's overall mark (use average)
-
-# class CFGStudent(Student):
-#
-#     def __init__(self, subject, grade):
-#         super().__init__(subject="History", grade=98)
-#
-#     def add_subject(self, subject, grade=int):
-#         self.subjects.update({subject:grade})
-#         print(f"{subject} subject has been added to {self.subjects}")
-#
-# student1 = CFGStudent(name='Jess', age=23, id=1234, grade=88)
-#
-# student.add.subject('History', 78)
-#
-#
-# print(student1)
+student_1.remove_subject('Lists')
+student_1.view_subjects()
